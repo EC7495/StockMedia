dbc6f70c819550d54c4946e92d40a532baef718f
Erick <ecanals07@gmail.com>
2020-07-15 18:49:40 -0400

    set up boilerplater for react and express app

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const morgan = require('morgan')
+const PORT = process.env.PORT || 8080
+
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, '..', 'public')))
+
+app.use('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public/index.html'))
+})
+
+app.use((err, req, res, next) => {
+  console.error(err)
+  console.error(err.stack)
+  res.status(err.status || 500).send(err.message || 'Internal server error.')
+})
+
+app.listen(PORT, () => {
+  console.log('Listening')
+})
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const isDev = process.env.NODE_ENV === 'development'
+const path = require('path')
+
+module.exports = {
+  mode: isDev ? 'development' : 'production',
+  entry: [
+    '@babel/polyfill', // enables async-await
+    path.join(__dirname, './index.jsx'),
+  ],
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  devtool: 'source-map',
+  watchOptions: {
+    ignored: /node_modules/,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+}
