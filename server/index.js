bc24d01d975748f42ec354c922fb52d96fe55973
Erick <ecanals07@gmail.com>
2020-07-15 19:09:17 -0400

    added redirect from http to https

--- server/index.js
+++ server/index.js
@@ -9,6 +9,14 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, '..', 'public')))
 
+if (process.env.NODE_ENV === 'production') {
+  app.use((req, res, next) => {
+    if (req.header('x-forwarded-proto') !== 'https')
+      res.redirect(`https://${req.header('host')}${req.url}`)
+    else next()
+  })
+}
+
 app.use('*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'public/index.html'))
 })
